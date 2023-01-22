6ddca5bc1746d331b4fc12adf088d169a4a30844
Bryan de Oliveira <bryan.oliveira12345@gmail.com>
2023-01-21 22:08:34 -0300

    Add files via upload

---
+++ FF7_Python/ff7_busterSword.py
@@ -0,0 +1,62 @@
+import pyautogui
+import time
+
+def autoStart():
+    pyautogui.keyDown('v')
+    pyautogui.keyUp('v')
+    time.sleep(.3)
+    pyautogui.keyDown('enter')
+    pyautogui.keyUp('enter')
+    time.sleep(.3)
+
+def startMission():
+    print('\n - Começando Missão')
+    autoStart()
+    autoStart()
+    autoStart()
+    autoStart()
+    autoBattle()
+
+def battleKeyboard():
+    time.sleep(1)
+    pyautogui.keyDown('e')
+    pyautogui.keyDown('i')
+    time.sleep(2)
+    pyautogui.keyDown('i')
+    pyautogui.keyUp('e')
+    pyautogui.keyDown('f')
+    pyautogui.keyDown('i')
+    pyautogui.keyUp('f')
+
+def autoBattle():
+    time.sleep(6.2)
+    count = 0
+    while count != 6:
+        battleKeyboard()
+        count += 1
+        print(f' - Executou Função battleKeyboard() {count} Vezes')
+        if count == 6:
+            pyautogui.keyDown('esc')
+            pyautogui.keyUp('esc')
+            time.sleep(15.5)
+            startMission()
+
+def startApp():
+    try:
+        start = str(input('\nComeçar ? \n\n''Y - Sim ; N - Não\n\n''= '))
+        start.lower()
+        if start == 'y':
+            print('\nAbra o Jogo !')
+            i = 5
+            while i >= 0:
+                time.sleep(1)
+                print(f' - Começando em {i} !')
+                i -= 1
+            startMission()
+        else: 
+            print("\n - ok")
+            startApp()
+    except:
+        startApp()
+
+startApp()
